e29b1839bed9e7e952755f791b3d74b2651da8de
Willems Frédéric <f.willems@student.helmo.be>
2023-04-30 12:19:30 +0200

    Fix dependencies ->     downgrading from pyasn1==0.5.0 to pyasn1==0.4.8
    Now using fork version of pysnmp from https://github.com/lextudio/pysnmp

--- request/request.py
+++ request/request.py
@@ -1,22 +1,24 @@
 from pysnmp.hlapi import *
-from pysnmp.entity.rfc3413.oneliner import cmdgen
 
-def snmp_get_info_test1(address:str, community: str, oid:str ) -> list:
-    cmdGen = cmdgen.CommandGenerator()
-    errorIndication, errorStatus, varBinds = cmdGen.getCmd(
-        cmdgen.CommunityData(community),
-        cmdgen.UdpTransportTarget((address, 161)),
-        oid
-    )
-def snmp_get_info(host:str, community: str, oid:str ) -> list:
+
+def snmp_get_info(host: str, community: str, oid: (tuple or str)) -> any:
     errorIndication, errorStatus, errorIndex, varBinds = next(
         getCmd(SnmpEngine(),
                CommunityData(community),
                UdpTransportTarget((host, 161)),
                ContextData(),
-               ObjectType(ObjectIdentity(oid))))
-    return varBinds
+               ObjectType(ObjectIdentity(*oid)) if type(oid) is tuple else ObjectType(ObjectIdentity(oid)))
+    )
+
+    if errorIndication:
+        print(errorIndication)
+        return None
+    elif errorStatus:
+        print(f"OID={oid} gave an error of type {errorStatus.prettyPrint()}")
+        return None
+    else:
+        return varBinds[0][1].prettyPrint()
 
 
-snmp_get_info("192.168.128.24", "HELMpAllUser9465CmA", "1.3.6.1.2.1.2.2.1.10.1")
-
+print(f'[INT]Octets IN for interface 1 = {snmp_get_info("192.168.128.24", "HELMpAllUser9465CmA", "1.3.6.1.2.1.2.2.1.10.1")}')
+print(f'[STR]Octets IN for interface 1 = {snmp_get_info("192.168.128.24", "HELMpAllUser9465CmA", ("IF-MIB", "ifInOctets", 1))}')
